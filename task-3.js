711a34de3914cf61d3fd28c0de367f6ab428e5c1
AdamZyla <azyla@interia.pl>
2024-08-06 20:37:19 +0200

    task 3 js

--- task-3.js
+++ task-3.js
@@ -1,2 +1,33 @@
 /* Zadanie 3. Profil gracza */
 
+const profile = {
+    username: "Jacob",
+    playTime: 300,
+  
+    changeUsername(newName) {
+      this.username = newName;
+    },
+  
+    updatePlayTime(hours) {
+      this.playTime += hours;
+    },
+  
+    getInfo() {
+      return `${this.username} has ${this.playTime} active hours!`;
+    }
+  };
+  
+  console.log(profile.getInfo()); 
+  
+  profile.changeUsername("Marco");
+  console.log(profile.getInfo()); 
+  
+  profile.updatePlayTime(20);
+  console.log(profile.getInfo()); 
+
+
+  /*
+        Jacob has 300 active hours!
+VM104:23 Marco has 300 active hours!
+VM104:26 Marco has 320 active hours!
+undefined */
